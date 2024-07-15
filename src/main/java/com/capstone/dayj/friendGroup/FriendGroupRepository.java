91f8599dfe4599aa049268921f665fff329690d4
Hose <tlswogh615@naver.com>
2024-07-16 02:23:07 +0900

    friendGroup 목표,달성여부,이메일 추가
    friendGroup의 친구 그룹 나가기, 친구 그룹내의 목표 내용과 목표 달성여부를
    체크할 수 있는 기능 추가. 또한 이메일 친구 불러오기 기능 추가
    X

--- src/main/java/com/capstone/dayj/friendGroup/FriendGroupRepository.java
+++ src/main/java/com/capstone/dayj/friendGroup/FriendGroupRepository.java
@@ -3,6 +3,9 @@ package com.capstone.dayj.friendGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FriendGroupRepository extends JpaRepository<FriendGroup, Integer> {
+    Optional<FriendGroup> findByEmail(String email);
 }
