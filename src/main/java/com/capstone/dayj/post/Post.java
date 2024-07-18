3e4effa206e6f31d1f7351f333b27ea04a46dceb
shl <shinhyung012@naver.com>
2024-07-18 22:11:47 +0900

    cascade 수정

--- src/main/java/com/capstone/dayj/post/Post.java
+++ src/main/java/com/capstone/dayj/post/Post.java
@@ -39,7 +39,7 @@ public class Post extends BaseEntity {
     @Column
     private String postPhoto;
     
-    @OneToMany(mappedBy = "post", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @OneToMany(mappedBy = "post", cascade = {CascadeType.REMOVE})
     private List<Comment> comment;
     
     @ManyToOne(fetch = FetchType.LAZY)
