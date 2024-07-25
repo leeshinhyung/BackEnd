9889b28db47204c92b32e225be4f2faf16517afb
DNA-B <sphz3813@gmail.com>
2024-07-25 21:58:39 +0900

    test2

--- src/main/java/com/capstone/dayj/setting/Setting.java
+++ src/main/java/com/capstone/dayj/setting/Setting.java
@@ -23,7 +23,7 @@ public class Setting extends BaseEntity {
     @ColumnDefault("0")
     private boolean alarm;
     
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "app_user_id", referencedColumnName = "id")
     @JsonIgnore
     private AppUser appUser;
